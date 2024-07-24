238ab89e6bb1aa5d06eb346ae4a1272621288888
belo <69517797+beloele@users.noreply.github.com>
2024-07-24 15:36:01 +0300

    dsfsd
    gadfga

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Grandpashabet: Canlı Bahis, Spor Bahisleri ve Casino - Grandpashabet Giriş",
+  description: " Grandpashabet, yüksek oranlar ve cazip bonuslarla bahis tutkunlarının gözdesi olmuştur. Futbol, basketbol, tenis gibi popüler spor dallarında geniş bahis seçenekleri sunan site, aynı zamanda canlı bahis imkanı da sunar. Grandpashabet, spor bahisleri ve casino oyunlarında eşsiz bir deneyim sunar.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
