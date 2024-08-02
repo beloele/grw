0d893c5e64d20bf391aeb3392d885001ead72511
belo <69517797+beloele@users.noreply.github.com>
2024-08-02 16:24:23 +0300

    adfgadg
    adfg

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Grandpashabet Güncel Giriş Adresi 2024",
   description: "Grandpashabet, yüksek oranlar ve cazip bonuslarla bahis tutkunlarının gözdesi olmuştur. Futbol, basketbol, tenis gibi popüler spor dallarında geniş bahis seçenekleri sunan site, aynı zamanda canlı bahis imkanı da sunar. Grandpashabet, spor bahisleri ve casino oyunlarında eşsiz bir deneyim sunar.",
-  keywords: "Grandpashabet, canlı bahis, spor bahisleri, casino, yüksek oranlar, bonus, futbol bahisleri, basketbol bahisleri, tenis bahisleri",
+  keywords: "grandpashabet, canlı bahis, spor bahisleri, casino, yüksek oranlar, bonus, futbol bahisleri, basketbol bahisleri, tenis bahisleri",
   authors: [{ name: "Grandpashabet", url: "https://grandpashabet.vercel.app" }],
   viewport: "width=device-width, initial-scale=1.0",
   robots: "index, follow",
   openGraph: {
     title: "Grandpashabet Güncel Giriş Adresi 2024",
     description: "Grandpashabet, yüksek oranlar ve cazip bonuslarla bahis tutkunlarının gözdesi olmuştur. Futbol, basketbol, tenis gibi popüler spor dallarında geniş bahis seçenekleri sunan site, aynı zamanda canlı bahis imkanı da sunar.",
-    url: "https://Grandpashabet.netlify.app",
+    url: "https://grandpashabet.vercel.app",
     type: "website",
     images: [
       {
