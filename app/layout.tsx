24bf8e24657bbe8843016f1fdb8f301d5dcf4b21
belo <69517797+beloele@users.noreply.github.com>
2024-08-02 16:21:31 +0300

    aaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,48 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Grandpashabet: Canlı Bahis, Spor Bahisleri ve Casino - Grandpashabet Giriş",
-  description: " Grandpashabet, yüksek oranlar ve cazip bonuslarla bahis tutkunlarının gözdesi olmuştur. Futbol, basketbol, tenis gibi popüler spor dallarında geniş bahis seçenekleri sunan site, aynı zamanda canlı bahis imkanı da sunar. Grandpashabet, spor bahisleri ve casino oyunlarında eşsiz bir deneyim sunar.",
+  title: "Grandpashabet Güncel Giriş Adresi 2024",
+  description: "Grandpashabet, yüksek oranlar ve cazip bonuslarla bahis tutkunlarının gözdesi olmuştur. Futbol, basketbol, tenis gibi popüler spor dallarında geniş bahis seçenekleri sunan site, aynı zamanda canlı bahis imkanı da sunar. Grandpashabet, spor bahisleri ve casino oyunlarında eşsiz bir deneyim sunar.",
+  keywords: "Grandpashabet, canlı bahis, spor bahisleri, casino, yüksek oranlar, bonus, futbol bahisleri, basketbol bahisleri, tenis bahisleri",
+  authors: [{ name: "Grandpashabet", url: "https://grandpashabet.vercel.app" }],
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
+  openGraph: {
+    title: "Grandpashabet Güncel Giriş Adresi 2024",
+    description: "Grandpashabet, yüksek oranlar ve cazip bonuslarla bahis tutkunlarının gözdesi olmuştur. Futbol, basketbol, tenis gibi popüler spor dallarında geniş bahis seçenekleri sunan site, aynı zamanda canlı bahis imkanı da sunar.",
+    url: "https://Grandpashabet.netlify.app",
+    type: "website",
+    images: [
+      {
+        url: "https://grandpashabet.vercel.app/logo.png",
+        width: 800,
+        height: 600,
+        alt: "Grandpashabet logo",
+      },
+    ],
+    siteName: "Grandpashabet",
+    locale: "tr_TR",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@Grandpashabetguncels",
+    creator: "@Grandpashabetguncels",
+    title: "Grandpashabet Güncel Giriş Adresi 2024",
+    description: "Grandpashabet, yüksek oranlar ve cazip bonuslarla bahis tutkunlarının gözdesi olmuştur. Futbol, basketbol, tenis gibi popüler spor dallarında geniş bahis seçenekleri sunan site, aynı zamanda canlı bahis imkanı da sunar.",
+    images: [
+      {
+        url: "https://grandpashabet.vercel.app/logo.png",
+        alt: "Grandpashabet logo",
+      },
+    ],
+  },
 };
+
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -4,31 +4,52 @@ import { useEffect } from "react";
 import Image from "next/image";
 import Link from 'next/link';
 
+
 export default function Home() {
-  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
-      <h1 className="text-4xl font-bold text-center mb-6">Grandpashabet Güncel Giriş</h1>
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] mb-8">
+      <h1 className="text-4xl font-bold text-center mb-6">Grandpashabet Güncel Giriş 2024</h1>
+      <div className="relative z-[1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] mb-8">
         <Link href="#" passHref>
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-            src="/logo.gif"
-            alt="Grandpashabet logo"
-            width={540}
-            height={111}
-            priority
-          />
+          <a>
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+              src="/logo.gif"
+              alt="Grandpashabet Guncel Giris Adresi"
+              width={350}
+              height={350}
+              priority
+            />
+          </a>
         </Link>
       </div>
-      <div className="mt-8">
-          <Link href="#" passHref>
-            <a className="inline-block px-8 py-4 bg-[#ff6e01] text-white font-semibold rounded-md transition-transform duration-300 transform hover:scale-105 hover:bg-[#4891ff]">
-              Grandpashabet Giriş İçin Tıkla
-            </a>
-          </Link>
-        </div>
-      <div className="text-center max-w-2xl mx-auto">
+      <div className="relative z-[1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] mb-8">
+        <Link href="#" passHref>
+          <a>
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+              src="/giris.png"
+              alt="Grandpashabet Giriş"
+              width={350}
+              height={200}
+              priority
+            />
+          </a>
+        </Link>
+      </div>
+      <div className="mt-8" style={{
+        marginTop: 40
+      }}>
+      
+        <Link href="#" passHref>
+          <a className="inline-block px-8 py-4 bg-[#344200] text-white font-semibold rounded-md transition-transform duration-300 transform hover:scale-105 hover:bg-[#98c200]">
+            Grandpashabet Giriş İçin Tıkla
+          </a>
+        </Link>
+      </div>
+      <div className="text-center max-w-2xl mx-auto"  style={{
+        marginTop: 40
+      }}>
         <section className="mb-4">
           <h2 className="text-2xl font-semibold mb-2">Grandpashabet Nedir?</h2>
           <p className="text-sm md:text-base">
@@ -63,8 +84,6 @@ export default function Home() {
             Grandpashabet, kullanıcı güvenliğini ve veri korumasını ön planda tutan bir bahis sitesidir. Modern şifreleme teknolojileri ile kullanıcı bilgilerini korur ve güvenli bir oyun ortamı sağlar. Sorumlu oyun ilkelerine bağlı kalarak, kullanıcılarına adil ve şeffaf bir oyun deneyimi sunar.
           </p>
         </section>
-        
-        
       </div>
     </main>
   );
